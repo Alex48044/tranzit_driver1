1fb81a34883b335cbfdd1a681b477874a2f99ae9
lexa-tranzit <lexa-koleso@mail.ru>
2026-06-07 21:29:05 +0300

    Fix: Explicitly pass Firebase options to initializeApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'services/feature_service.dart';
+import 'firebase_options.dart';
+import 'feature_flags.dart';
 import 'screens/main_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  final featureService = FeatureService();
-  await featureService.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FeatureFlags().init();
   runApp(const TranzitApp());
-}
-
-class TranzitApp extends StatelessWidget {
-  const TranzitApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Транзит Водитель',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MainScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
 }
